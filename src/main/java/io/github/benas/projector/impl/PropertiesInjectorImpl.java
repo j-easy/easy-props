81fb20c94f2e79db821c0ca4c2ffb2a429a3d9c8
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-01-29 21:09:50 +0100

    change base package name to match new project name

---
+++ src/main/java/io/github/benas/projector/impl/PropertiesInjectorImpl.java
@@ -0,0 +1,106 @@
+/*
+ *   The MIT License
+ *
+ *    Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *    THE SOFTWARE.
+ */
+
+package io.github.benas.projector.impl;
+
+import io.github.benas.projector.annotations.*;
+import io.github.benas.projector.annotations.Properties;
+import io.github.benas.projector.api.AnnotationProcessor;
+import io.github.benas.projector.api.PropertiesInjector;
+import io.github.benas.projector.processors.*;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.*;
+
+/**
+ * The core implementation of the {@link io.github.benas.projector.api.PropertiesInjector} interface.
+ *
+ * @author Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ */
+final class PropertiesInjectorImpl implements PropertiesInjector {
+
+    /**
+     * A map holding registered annotations with their processors.
+     */
+    private Map<Class<? extends Annotation>, AnnotationProcessor> annotationProcessors;
+
+    /**
+     * Public constructor.
+     */
+    public PropertiesInjectorImpl() {
+        annotationProcessors = new HashMap<Class<? extends Annotation>, AnnotationProcessor>();
+        annotationProcessors.put(SystemProperty.class, new SystemPropertyAnnotationProcessor());
+        annotationProcessors.put(Property.class, new PropertyAnnotationProcessor());
+        annotationProcessors.put(I18NProperty.class, new I18NPropertyAnnotationProcessor());
+        annotationProcessors.put(Properties.class, new PropertiesAnnotationProcessor());
+        annotationProcessors.put(DBProperty.class, new DBPropertyAnnotationProcessor());
+        annotationProcessors.put(JNDIProperty.class, new JNDIPropertyAnnotationProcessor());
+        annotationProcessors.put(MavenProperty.class, new MavenPropertyAnnotationProcessor());
+        annotationProcessors.put(ManifestProperty.class, new ManifestPropertyAnnotationProcessor());
+    }
+
+    @Override
+    public void injectProperties(Object object) throws Exception {
+
+        /*
+         * Retrieve declared fields
+         */
+        List<Field> declaredFields = new ArrayList<Field>(Arrays.asList(object.getClass().getDeclaredFields()));
+
+        /*
+         * Retrieve inherited fields for all type hierarchy
+         */
+        Class clazz = object.getClass();
+        while (clazz.getSuperclass() != null) {
+            Class superclass = clazz.getSuperclass();
+            declaredFields.addAll(Arrays.asList(superclass.getDeclaredFields()));
+            clazz = superclass;
+        }
+
+        /*
+         * Introspect fields for each registered annotation, and delegate it processing to the right annotation processor
+         */
+        for (Field field : declaredFields) {
+            for (Class<? extends Annotation> annotationType : annotationProcessors.keySet()) {
+                AnnotationProcessor annotationProcessor = annotationProcessors.get(annotationType);
+                if (field.isAnnotationPresent(annotationType) && annotationProcessor != null) {
+                    Annotation annotation = field.getAnnotation(annotationType);
+                    annotationProcessor.processAnnotation(annotation, field, object);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Register a custom annotation processor for a given annotation.
+     * @param annotation the annotation type to be processed
+     * @param annotationProcessor the annotation processor to register
+     */
+    public void registerAnnotationProcessor(final Class<? extends Annotation> annotation,final AnnotationProcessor annotationProcessor) {
+        annotationProcessors.put(annotation, annotationProcessor);
+    }
+
+}
---
+++ src/main/java/io/github/benas/projector/processors/PropertiesAnnotationProcessor.java
@@ -0,0 +1,83 @@
+/*
+ *   The MIT License
+ *
+ *    Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *    THE SOFTWARE.
+ */
+
+package io.github.benas.projector.processors;
+
+import io.github.benas.projector.annotations.Properties;
+import io.github.benas.projector.api.AnnotationProcessor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An annotation processor that loads all properties from a properties file.
+ *
+ * @author Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ */
+public class PropertiesAnnotationProcessor extends AbstractAnnotationProcessor implements AnnotationProcessor<Properties> {
+
+    /**
+     * A map holding source file name and Properties object serving as a cache.
+     */
+    private Map<String, java.util.Properties> propertiesMap = new HashMap<String, java.util.Properties>();
+
+    @Override
+    public void processAnnotation(final Properties propertiesAnnotation, final Field field, Object object) throws Exception {
+
+        if (!field.getType().equals(java.util.Properties.class)) {
+            throw new Exception("@Properties declared on field " + field.getName() + " of type " +
+                    object.getClass() + " is incompatible with type " + field.getType());
+        }
+
+        String source = propertiesAnnotation.value().trim();
+
+        if (source.isEmpty()) {
+            throw new Exception(missingAttributeValue("source", "@Properties", field, object));
+        }
+
+
+        //check if the source file is not already loaded
+        if (!propertiesMap.containsKey(source)) {
+            java.util.Properties properties = new java.util.Properties();
+            try {
+                InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(source);
+                if (inputStream != null) {
+                    properties.load(inputStream);
+                    propertiesMap.put(source, properties);
+                } else {
+                    throw new Exception(missingSourceFile(source, field, object));
+                }
+            } catch (IOException ex) {
+                throw new Exception(missingSourceFile(source, field, object), ex);
+            }
+        }
+
+        injectProperty(object, field, source, propertiesMap.get(source));
+
+    }
+
+}
