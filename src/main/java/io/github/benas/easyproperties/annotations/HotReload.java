14a1fcc4162e4fb7ffb810ed2745e17bf64d3ec7
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-07-01 15:58:13 +0200

    add time unit attribute to HotReload annotation

--- src/main/java/io/github/benas/easyproperties/annotations/HotReload.java
+++ src/main/java/io/github/benas/easyproperties/annotations/HotReload.java
@@ -4,16 +4,31 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * Annotation to mark an object for hot configuration reloading.
+ *
+ * This will register a background thread to reload the configuration periodically in the target object.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface HotReload {
 
     /**
-     * The period (in ms) of configuration reloading.
+     * The period to wait before reloading configuration.
      *
-     * @return period (in ms) of reloading
+     * @return The period to wait before reloading configuration
      */
-    long period() default 60 * 1000;
+    long period() default 15;
+
+    /**
+     * Time unit of the waiting period.
+     *
+     * @return Time unit of the waiting period
+     */
+    TimeUnit unit() default TimeUnit.MINUTES;
 
 }
