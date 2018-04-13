7eaef205c4b90477b30b905cd7d7eacb1271248e
Michael Couck <michael.couck@gmail.com>
2018-04-13 11:26:51 +0200

    Update AbstractAnnotationProcessor.java

--- src/main/java/org/jeasy/props/processors/AbstractAnnotationProcessor.java
+++ src/main/java/org/jeasy/props/processors/AbstractAnnotationProcessor.java
@@ -1,8 +1,8 @@
-/*
+/**
  * The MIT License
- * <p>
+ * 
  * Copyright (c) 2017, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
- * <p>
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
